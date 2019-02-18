33d2416eeb016124b0189c30a6b5a0c02e8f7063
ymtwithcarp <kkcymt@yahoo.co.jp>
2019-02-18 15:39:41 +0900

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ set :repo_url, "git@github.com:ymtwithcarp/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 # set :linked_files, %w{ config/secrets.yml }
 
-set :rbenv_type, :system
+set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
